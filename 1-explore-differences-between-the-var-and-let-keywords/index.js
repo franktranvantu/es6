777a95836b298a85b854682bf44ccadda23ed793
frank <franktranvantu@gmail.com>
2020-12-19 13:46:33 +0700

    update

---
+++ 1-explore-differences-between-the-var-and-let-keywords/index.js
@@ -0,0 +1,14 @@
+var catName = "Quincy";
+// let catName = "Quincy";
+var quote;
+
+var catName = "Beau";
+
+function catTalk() {
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
+
+}
+catTalk();
